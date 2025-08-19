41b79dd40444c6830c8df6ad7fcaa90291e3e4a6
hayden.rear <hayden.rear@gmail.com>
2025-08-19 18:56:49 -0400

    updated

---
+++ src/main/java/com/hayden/testmcpserver/TestMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.hayden.testmcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMcpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hayden/testmcpserver/TestMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayden.testmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
